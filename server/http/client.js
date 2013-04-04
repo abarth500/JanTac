8ac54fd4d7bed8b4cd54c83db1f0390128da56ba
Shohei Yokoyama <shohei.yokoyama@gmail.com>
2013-04-04 09:51:49 +0900

    debug

--- server/http/client.js
+++ server/http/client.js
@@ -41,7 +41,8 @@ JanTac.Manager = function(){
 				console.log(message.data); // test
   			};
 		}
-		
+		this.ws..onclose = function(evt) { console.log("\tClose:"+JSON.stringify(evt));};  
+		this.ws..onerror = function(evt) { console.log("\tERROR:"+JSON.stringify(evt)); };
 	}
 	this.setBehavior = function(runLevel){
 		this.clearBehavior();
